af5343007f38441a323c6b5947980b1b0eaf1015
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-02-09 02:37:56 -0600

    Fixed map default

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic2xvYmFud2FsYSIsImEiOiJja2t4cTh4enUxcjcwMnBwaDJ3dW92dTY0In0.N1tRRdo0xexfj8EdoMWZ0A";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,12 @@
+// Name: Shanil Lobanwala
+// Date: Feb 5-6, 2021
+// Project Name: leaflet-challenge
+// File: leaflet-challenge/Leaflet-Step-1/logic.js
+
+// Function to set up map with the center and zoom value
+var initMap = L.map("map", {
+  center: [38, -123],
+  zoom: 4
+});
+
+// map tile layer(the background)
