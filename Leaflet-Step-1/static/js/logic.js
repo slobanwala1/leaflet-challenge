0e5216057da0751ac8c2d609eebad2d572d8ca69
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-02-09 03:40:11 -0600

    fixed colors

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -9,7 +9,7 @@ var initMap = L.map("map", {
   zoom: 4
 });
 
-// map tile layer(the background)
+// map tile layer(the background) use the api key here
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={your_access_token}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -19,35 +19,43 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   your_access_token: API_KEY
 }).addTo(initMap);
 
-// earthquake API
+// earthquake API, based on personal preference
 var api = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(api, function(data) {
+  // fill the circles with color and how big the radius is based on data returned.
   function styleMap(feature) {
     return {
-      opacity: 1,
-      fillOpacity: 1,
       fillColor: styleColor(feature.properties.mag),
       color: "#000000",
       radius: styleRadius(feature.properties.mag),
       stroke: true,
-      weight: 0.5
+      weight: 0.5,
+      opacity: 1,
+      fillOpacity: 1,
     };
   }
 
+  //
   function styleColor(mapFeatureMag) {
     if (mapFeatureMag > 5) {
-      return "#ea2c2c";
+      // red
+      return "#ff0000";
     } else if(mapFeatureMag > 4) {
-      return "#eaa92c";
+      // orange
+      return "#ffa500";
     } else if(mapFeatureMag > 3) {
-      return "#d5ea2c";
+      // yellow
+      return "#ffff00";
     } else if(mapFeatureMag > 2) {
-      return "#92ea2c";
+      // lime green
+      return "#32cd32";
     } else if(mapFeatureMag > 1) {
-      return "#2ceabf";
+      // green-cyan
+      return "#00bf80";
     } else {
-      return "#2c99ea";
+      // bright cyan
+      return "#2cc8eb";
     }
   }
 
@@ -78,10 +86,10 @@ d3.json(api, function(data) {
   mapLegend.onAdd = function() {
     var div = L.DomUtil.create("div", "legend");
 
-    // Magnitude grades and associated colors
+    // Magnitude grades and associated colors, grabbed from styleColor func
     var magGrades = [0, 1, 2, 3, 4, 5];
 
-    var magColors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+    var magColors = ["#2cc8eb", "#00bf80", "#32cd32", "#ffff00", "#ffa500", "#ff0000"];
 
     for(var i = 0; i < magGrades.length; i++) {
       div.innerHTML += "<i style='background: " + magColors[i] + "'></i> " +
